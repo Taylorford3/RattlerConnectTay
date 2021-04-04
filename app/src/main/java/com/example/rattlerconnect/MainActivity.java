ae3e130e967b1184eda048288bf7296b791aecab
Taylor Ford <taylor@Taylors-MacBook-Air-2.local>
2021-04-04 02:45:31 -0400

    Initial commit

---
+++ app/src/main/java/com/example/rattlerconnect/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.rattlerconnect;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.rattlerconnect.Fragments.ComposeFragment;
+import com.example.rattlerconnect.Fragments.MessageFragment;
+import com.example.rattlerconnect.Fragments.PostsFragment;
+import com.example.rattlerconnect.Fragments.ProfileFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+
+        // define your fragments here
+        final Fragment postsfragment    = new PostsFragment();
+        final Fragment messagefragment = new MessageFragment();
+        final Fragment composefragment = new ComposeFragment();
+        final Fragment profilefragment = new ProfileFragment();
+
+
+        // handle navigation selection
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener( new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment;
+                switch (item.getItemId()) {
+                    case R.id.action_home:
+                         fragment = new PostsFragment();
+                        break;
+                    case R.id.action_message:
+                        fragment = new MessageFragment();
+                        break;
+                    case R.id.action_compose:
+                        fragment = new ComposeFragment();
+                        break;
+                    case R.id.action_profile:
+                        fragment = new ProfileFragment();
+                    default:
+                        fragment = new PostsFragment();
+                        break;
+                }
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                return true;
+            }
+        });
+        // Set default selection
+        bottomNavigationView.setSelectedItemId(R.id.action_home);
+    }
+}
